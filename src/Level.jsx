6c4506ba5b53d44f12f3c962cdde2b0da7496beb
Cooper Simpson <cooper@coopersimpson.com>
2024-01-27 14:33:57 +1000

    Add BlockLimo obstacle

--- src/Level.jsx
+++ src/Level.jsx
@@ -58,10 +58,44 @@ function BlockSpinner( {position = [0, 0, 0]} ) {
     </group>
 }
 
+function BlockLimbo( {position = [0, 0, 0]} ) {
+    const obstacle = useRef();
+    const [ timeOffset ] = useState(() => Math.random() * Math.PI * 2);
+
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime();
+        
+        const y = Math.sin(time + timeOffset) + 1.15;
+        obstacle.current.setNextKinematicTranslation( { x:position[0], y:position[1] + y, z:position[2]})
+    })
+
+    return <group position = { position }>
+        {/* Floor */}
+        <mesh 
+            geometry= { boxGeometry } 
+            material={ floor2Material } 
+            position={ [0, -0.1, 0 ]} 
+            scale={ [4, 0.2, 4] } 
+            receiveShadow>
+        </mesh>
+            
+        {/* Limbo obstacle */}
+        <RigidBody ref={ obstacle } type="kinematicPosition" position={ [0, 0.3, 0] } restitution={ 0.2 } friction={ 0 }>
+            <mesh 
+                geometry= { boxGeometry } 
+                material={ obstacleMaterial } 
+                scale={ [3.5, 0.3, 0.3] }
+                castShadow
+                receiveShadow>
+            </mesh>
+        </RigidBody>
+    </group>
+}
 
 export default function Level() {
   return <>
-        <BlockStart position={ [0, 0, 4] }/>
-        <BlockSpinner position={ [0, 0, 0] }/>
+        <BlockStart position={ [0, 0, 8] }/>
+        <BlockSpinner position={ [0, 0, 4] }/>
+        <BlockLimbo position={ [0, 0, 0] }/>
     </>
 }
