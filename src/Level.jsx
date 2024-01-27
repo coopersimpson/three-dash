6ee87c5d7f655169f790d96bbca12d235b675305
Cooper Simpson <cooper@coopersimpson.com>
2024-01-27 20:51:53 +1000

    Randomise blocks

--- src/Level.jsx
+++ src/Level.jsx
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { RigidBody } from '@react-three/rapier';
-import { useState, useRef } from 'react';
+import { useMemo, useState, useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
 
@@ -11,7 +11,7 @@ const floor2Material = new THREE.MeshToonMaterial({ color: 'cyan' });
 const obstacleMaterial = new THREE.MeshToonMaterial({ color: 'orangered' });
 const wallMaterial = new THREE.MeshToonMaterial({ color: 'grey' });
 
-function BlockStart( {position = [0, 0, 0]} ) {
+export function BlockStart( {position = [0, 0, 0]} ) {
     return <group position = { position }>
         {/* Floor */}
         <mesh 
@@ -24,7 +24,7 @@ function BlockStart( {position = [0, 0, 0]} ) {
     </group>
 }
 
-function BlockEnd( {position = [0, 0, 0]} ) {
+export function BlockEnd( {position = [0, 0, 0]} ) {
 
     const relic = useGLTF('/relic.glb');
 
@@ -51,7 +51,7 @@ function BlockEnd( {position = [0, 0, 0]} ) {
     </group>
 }
 
-function BlockSpinner( {position = [0, 0, 0]} ) {
+export function BlockSpinner( {position = [0, 0, 0]} ) {
     const obstacle = useRef();
     const [ speed ] = useState(() => (Math.random() + 0.2) * (Math.random() < 0.5 ? -1 : 1));
 
@@ -86,7 +86,7 @@ function BlockSpinner( {position = [0, 0, 0]} ) {
     </group>
 }
 
-function BlockLimbo( {position = [0, 0, 0]} ) {
+export function BlockLimbo( {position = [0, 0, 0]} ) {
     const obstacle = useRef();
     const [ timeOffset ] = useState(() => Math.random() * Math.PI * 2);
 
@@ -120,7 +120,7 @@ function BlockLimbo( {position = [0, 0, 0]} ) {
     </group>
 }
 
-function BlockAxe( {position = [0, 0, 0]} ) {
+export function BlockAxe( {position = [0, 0, 0]} ) {
     const obstacle = useRef();
     const [ timeOffset ] = useState(() => Math.random() * Math.PI * 2);
 
@@ -154,13 +154,22 @@ function BlockAxe( {position = [0, 0, 0]} ) {
     </group>
 }
 
-export default function Level() {
-  return <>
-        <BlockStart position={ [0, 0, 16] }/>
-        <BlockSpinner position={ [0, 0, 12] }/>
-        <BlockLimbo position={ [0, 0, 8] }/>
-        <BlockAxe position={ [0, 0, 4] }/>
-        <BlockEnd position={ [0, 0, 0] }/>
+export function Level( { count = 5, types = [ BlockSpinner, BlockAxe, BlockLimbo ] }) {
 
-    </>
-}
+    const blocks = useMemo(() => {
+        const blocks = [];
+
+        for (let i = 0; i < count; i++) {
+            const type = types[ Math.floor(Math.random() * types.length)];
+            blocks.push(type);
+        }
+
+        return blocks;
+    }, [ count, types ])
+
+    return <>
+            <BlockStart position={ [0, 0, 0] }/>
+            { blocks.map((Block, index) => <Block key={ index } position={ [ 0, 0, - (index + 1) * 4 ]} />) }
+            <BlockEnd position={ [ 0, 0, - (count + 1) * 4 ] }/>
+        </>
+    }
