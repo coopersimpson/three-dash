e8e367fea783cd4aadb3614a1e851e5530b7ca7b
Cooper Simpson <cooper@coopersimpson.com>
2024-01-28 13:50:32 +1000

    Add player

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -2,15 +2,17 @@ import { OrbitControls } from '@react-three/drei'
 import { Physics } from '@react-three/rapier'
 import Lights from './Lights.jsx'
 import { Level } from './Level.jsx'
+import Player from './Player.jsx'
 
 export default function Experience()
 {
     return <>
 
         <OrbitControls makeDefault />
-        <Physics debug>
+        <Physics>
             <Lights />
             <Level />
+            <Player />
         </Physics>
     </>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -3,20 +3,31 @@ import ReactDOM from 'react-dom/client'
 import { Canvas } from '@react-three/fiber'
 import Experience from './Experience.jsx'
 import { Perf } from 'r3f-perf'
+import { KeyboardControls } from '@react-three/drei'
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
-    <Canvas
-        shadows
-        camera={ {
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [ 2.5, 4, 6 ]
-        } }
+    <KeyboardControls
+        map={ [
+            { name: 'leftward', keys: ['KeyA', 'ArrowLeft'] },
+            { name: 'rightward', keys: ['KeyD', 'ArrowRight'] },
+            { name: 'forward', keys: ['KeyW', 'ArrowUp'] },
+            { name: 'backward', keys: ['KeyS', 'ArrowDown'] },
+            { name: 'jump', keys: ['Space'] },
+        ] }
     >
-        <Perf/>
-        <Experience />
-    </Canvas>
+        <Canvas
+            shadows
+            camera={ {
+                fov: 45,
+                near: 0.1,
+                far: 200,
+                position: [ 2.5, 4, 6 ]
+            } }
+        >
+            <Perf/>
+            <Experience />
+        </Canvas>
+    </KeyboardControls>
 )
